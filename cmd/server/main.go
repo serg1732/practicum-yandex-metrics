253df7c34b5666885b9ad83ec64bb122fa9b9268
sergey.miron <sergey.miron@rtlabs.ru>
2025-12-21 18:23:33 +0300

    fix: test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/serg1732/practicum-yandex-metrics/internal/handler"
@@ -11,7 +10,6 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /update/{metricType}/{metricName}/{metricValue}", handler.UpdateHandler)
 	mux.HandleFunc("POST /update/{metricType}/{metricValue}", http.NotFound)
-	fmt.Println("test")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		panic(err)
